9c86a7577ff74e9df25f500759cc73c0de280c9e
Orel-doudou-records <orellam31@gmail.com>
2026-09-11 21:36:36 +0200

    feat: export collaborative core versioning API

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from "./collaborative-core/index.js";
+export * from "./collaborative-core/versioning.js";
 export * from "./diffract/index.js";
 export * from "./litcraft/index.js";
 export * from "./lunette-ronde/index.js";
