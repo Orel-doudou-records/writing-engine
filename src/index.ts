43af72027a27a3b883873e3ff0bf1f9739061e36
Orel-doudou-records <orellam31@gmail.com>
2026-09-11 22:06:49 +0200

    feat: expose conflict-aware proposal integration

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,29 @@
 export * from "./collaborative-core/index.js";
 export * from "./collaborative-core/versioning.js";
 export * from "./collaborative-core/collaboration.js";
-export * from "./collaborative-core/proposal.js";
+export {
+  ProposalStatusSchema,
+  ReviewDecisionKindSchema,
+  ProposalItemSchema,
+  ReviewDecisionSchema,
+  ProposalSchema,
+  IntegrationSchema,
+  createProposal,
+  submitProposal,
+  reviewProposalSelection,
+  type ProposalStatus,
+  type ReviewDecisionKind,
+  type ProposalItem,
+  type ReviewDecision,
+  type Proposal,
+  type Integration,
+  type ProposalItemInput,
+  type CreateProposalInput,
+  type ReviewProposalSelectionInput,
+  type IntegrateProposalInput,
+  type IntegrateProposalResult,
+} from "./collaborative-core/proposal.js";
+export * from "./collaborative-core/conflicts.js";
 export * from "./diffract/index.js";
 export * from "./litcraft/index.js";
 export * from "./lunette-ronde/index.js";
