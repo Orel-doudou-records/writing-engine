304c78773f643cb53f6f08df7dbdf816fb52a414
Orel-doudou-records <orellam31@gmail.com>
2026-09-11 21:57:38 +0200

    feat: export proposal review API

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from "./collaborative-core/index.js";
 export * from "./collaborative-core/versioning.js";
 export * from "./collaborative-core/collaboration.js";
+export * from "./collaborative-core/proposal.js";
 export * from "./diffract/index.js";
 export * from "./litcraft/index.js";
 export * from "./lunette-ronde/index.js";
